13b46c2591131917bb4418525d3bf883722f21d1
Bombale <amruth.parasuram@bt.com>
2019-05-27 21:54:09 +0530

    Add one more greeting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1><br><h2>Hello World, but smaller</h2>');
+    res.end('<h1>Hello World</h1><br><h2>Hello World, but smaller</h2><br><br><p>Smallest hello world</p>');
 });
 server.listen(port, () => {
     console.log(`Server running at port ` + port);
